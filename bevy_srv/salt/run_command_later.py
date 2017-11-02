aa6ebbfc5c0faa9582436dca1ce1456de200310d
Vernon Cole <vernondcole@gmail.com>
2017-11-02 00:45:45 -0600

    older Python3 friendly

--- bevy_srv/salt/run_command_later.py
+++ bevy_srv/salt/run_command_later.py
@@ -43,7 +43,7 @@ for command in commands:
         print('{} running command-> {}'.format(__file__, command))
     sys.stdout.flush()
     try:
-        sp = subprocess.run(command, shell=True, check=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
+        sp = subprocess.check_call(command, shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
         retcd = sp.returncode
     except subprocess.CalledProcessError as e:
         print(e)
